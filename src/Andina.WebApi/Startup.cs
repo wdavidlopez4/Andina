d4e78155f4959a35963f86ec6d132d12f1d4d769
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2020-09-11 00:54:50 -0500

    se configuro el IOC en el webapp clase startup

--- src/Andina.WebApi/Startup.cs
+++ src/Andina.WebApi/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Andina.Infrastructure.InfrastructureIOC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,9 @@ namespace Andina.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //inyectar dependencias
+            RegistrarInyecciones(services, Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +50,16 @@ namespace Andina.WebApi
             {
                 endpoints.MapControllers();
             });
+        }
+
+        //injeccion de dependencias
+        private static void RegistrarInyecciones(IServiceCollection services, IConfiguration Configuration)
+        {
+            InjectionContainer.InyectarDbSettings(services, Configuration);
+
+            InjectionContainer.InyectarDbContext(services);
+            InjectionContainer.InyectarRepositorio(services);
+            InjectionContainer.InyectarServicios(services);
         }
     }
 }
