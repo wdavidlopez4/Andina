601871570667c411e6cbe557e9b73f22ac9ae4b9
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2020-09-10 23:03:56 -0500

    crear la interfaz repositorio en el modelo

---
+++ src/Andina.Domain/Models/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andina.Domain.Models.Interfaces
+{
+    public interface IRepository
+    {
+        public Task<T> Crear<T>();
+    }
+}
