33a6e0ef0b35a0a4cfa0abc3ba1f7540ae5d8487
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2020-09-10 22:59:23 -0500

    primer componente de servicio "Veiculo"

---
+++ src/Andina.Application/VeiculoServices/Interfaces/IVeiculoService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andina.Application.VeiculoServices.Interfaces
+{
+    public interface IVeiculoService
+    {
+        public Task<bool> GuardarVeiculo();
+    }
+}
---
+++ src/Andina.Application/VeiculoServices/VeiculoService.cs
@@ -0,0 +1,16 @@
+﻿using Andina.Application.VeiculoServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andina.Application.VeiculoServices
+{
+    class VeiculoService : IVeiculoService
+    {
+        public Task<bool> GuardarVeiculo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
