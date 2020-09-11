fc0aac6421e00f169944d0f2529d9d78654cdec1
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2020-09-10 22:48:45 -0500

    primeras clases de los modelos

---
+++ src/Andina.Domain/Models/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Andina.Domain.Models
+{
+    public class BaseModel
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Andina.Domain/Models/VeiculoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Andina.Domain.Models
+{
+    public class VeiculoModel : BaseModel
+    {
+        public string Placa { get; set; }
+
+        public string Marca { get; set; }
+
+        public string Estado { get; set; }
+    }
+}
