2f9f09fd6aedea04502076afd1c582ec593f060d
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2020-10-01 22:51:26 -0500

    agregar etiquetas de mapeo solo para mejorar los campos a la hora de mapear los documnetos de las colecciones.

--- src/Andina.Domain/Models/BaseModel.cs
+++ src/Andina.Domain/Models/BaseModel.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Andina.Domain.Models
 {
-    public class BaseModel
+    public abstract class BaseModel
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public Guid Id { get; set; }
     }
 }
--- src/Andina.Domain/Models/Usuarios/Usuario.cs
+++ src/Andina.Domain/Models/Usuarios/Usuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,22 +7,31 @@ namespace Andina.Domain.Models.Usuarios
 {
     public class Usuario : BaseModel
     {
+        [BsonElement("nombres")]
         public string Nombres { get; set; }
 
+        [BsonElement("apellidos")]
         public string Apellidos { get; set; }
 
+        [BsonElement("email")]
         public string Email { get; set; }
 
+        [BsonElement("contrasena")]
         public string Contraseña { get; set; }
 
+        [BsonElement("numeroIdentidad")]
         public int NumeroIdentidad { get; set; }
 
+        [BsonElement("tipoIdentidad")]
         public TipoIdentidad TipoIdentidad { get; set; }
 
+        [BsonElement("fechaExpedicionIdentidad")]
         public DateTime FechaExpedicionIdentidad { get; set; }
 
+        [BsonElement("fechaDeNacimineto")]
         public DateTime FechaDeNacimineto { get; set; }
 
+        [BsonElement("rolUsuario")]
         public Roles Rol { get; set; }
 
 
